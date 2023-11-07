4bb9cb33bf28852a590739c5952a1e39ffe09d85
Joao Gabriel <joaogabriel.farias@Hotmail.com>
2023-11-07 19:34:03 -0300

    Módulo 01 - Aula 01

---
+++ modulo01/aula01-mocks/src/constants.js
@@ -0,0 +1,10 @@
+// Este arquivo armazenará todos os erros da aplicação
+
+const constants = {
+  error: {
+    FILE_LENGTH_ERROR_MESSAGE: 'The content length is invalid.',
+    FILE_FIELDS_ERROR_MESSAGE: 'The provided properties are invalid',
+  }
+}
+
+module.exports = constants
